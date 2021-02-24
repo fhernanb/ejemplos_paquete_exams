61795bfa7ba175ceb2d950248c051e693f091eaa
fhernanb <fhernanb@unal.edu.co>
2021-02-24 18:33:36 -0500

    Se incluyó una pregunta que tiene respuestas como matrices

--- creador.R
+++ creador.R
@@ -1,6 +1,12 @@
 
 library("exams")
+
+# Para ver las preguntas en pdf
 exams2pdf("usando_dataframe.Rmd")
 exams2pdf("usando_knitr_para_tabla.Rmd")
 exams2pdf("estimando_b0_lm.Rmd")
 exams2pdf("estimando_b1_lm")
+
+# Para ver las preguntas en html
+exams2html("respuestas_como_matrices.Rmd", 
+           dir=".", mathjax=TRUE)
